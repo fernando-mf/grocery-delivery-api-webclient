969f8e6c8b0ff1c378f814366b960419ebe92e50
Fernando MF <prelt65@gmail.com>
2018-08-12 17:02:52 -0400

    Added bootstrap ts modules (reactstrap, bootstrap package) - Implemented base project directories structure - Added home component - Added sample shared component - Project cleanup

--- shipping-app/ClientApp/src/index.tsx
+++ shipping-app/ClientApp/src/index.tsx
@@ -1,7 +1,7 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import App from './Components/App/App';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
