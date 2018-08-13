02d821a8afdccd101afbdfaad0fdee3bfcd8167f
fernando-mf <fernando.1703@hotmail.com>
2018-08-12 21:02:03 -0400

    Changed app title

--- shipping-app/ClientApp/src/Components/App/App.tsx
+++ shipping-app/ClientApp/src/Components/App/App.tsx
@@ -7,7 +7,7 @@ class App extends React.Component {
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">Shipping App</h1>
+          <h1 className="App-title">Grocery Delivery App</h1>
         </header>
         <Home />
       </div>
